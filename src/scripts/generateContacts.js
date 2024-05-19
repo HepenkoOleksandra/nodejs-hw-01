2f92d0b4119e8e400b01c954c5acdbbdece6b0d7
HepenkoOleksandra <gepenkoalex@gmail.com>
2024-05-19 19:06:09 +0300

    generate

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,6 +1,17 @@
 import fs from 'node:fs/promises';
 import { PATH_DB } from '../constants/contacts.js';
+import { createFakeContact } from '../utils/createFakeContact.js';
 
-const generateContacts = async (number) => {};
+const generateContacts = async (number) => {
+    const data = await fs.readFile(PATH_DB, 'utf8');
+    const parseContacts = JSON.parse(data);
+
+    for (let i = 0; i < number; i++) {
+        parseContacts.push(createFakeContact());
+    }
+    const stringifyContacts = JSON.stringify(parseContacts);
+    await fs.writeFile(PATH_DB, stringifyContacts);
+    console.log(parseContacts);
+};
 
 await generateContacts(5);
